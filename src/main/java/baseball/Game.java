e31d88a652db29caff3d9351ff01bf1f65649d6c
KimJinYeongZ <cheche903@naver.com>
2024-10-14 12:50:59 +0900

    <feat>: 각 메서드 설명 및 메서드 틀 구성

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -11,9 +11,41 @@ public class Game {
     static ArrayList<Integer> computerNums;
 
 
+    /**
+     * Game 을 시작하는 start() 메서드
+     * Application 에서 인스턴스화 하여 이 메서드를 부름으로써 게임을 시작한다.
+     */
     public void start() {
+
+        boolean isStartGame = true;
+
+        while(isStartGame) {
+
+            computerNums = getComputerNums();
+
+            startRound();
+
+            isStartGame = checkStart();
+        }
     }
 
+
+    /**
+     * 라운드 진행 startRound() 메서드
+     */
+    public void startRound() {
+
+    }
+
+
+    /**
+     * 게임이 끝난 후, 재시작을 묻는 checkStart() 메서드
+     * @return Boolean 형 반환, true => 재시작, false => 종료
+     */
+    public boolean checkStart() {
+
+        return true;
+    }
 
     /**
      * 랜덤 수 3개를 갖는 리스트를 만드는 getComputerNums() 메서드
@@ -33,4 +65,15 @@ public class Game {
 
         return computerNums;
     }
+
+
+    /**
+     * 사용자 입력 예외를 잡는 checkException() 메서드
+     * 1. 게임 시작 및 종료 선택 시, 1 또는 2의 입력이 아닐 때
+     * 2. 게임 라운드 진행 시, 3개의 숫자가 아닌, 2개 이하, 4개 이상의 숫자가 입력될 때
+     */
+    public void checkException() {
+
+    }
+
 }
